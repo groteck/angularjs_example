8f521f01bbdefead2de9a872ce4069e31c08604e
groteck <groteck@gmail.com>
2013-05-26 06:24:49 +0100

    Site updated to 1e122ef

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -1,15 +1,28 @@
 (function() {
+  var blog;
 
-  angular.module('angularExample', []);
+  blog = angular.module('blog', ['ngResource']);
 
-  window.PostListController = function($scope, $http) {
-    var url;
-    url = "http://blog-test-angularjs.herokuapp.com/posts.json";
-    return $http.get(url).success(function(json) {
-      return $scope.posts = json;
-    });
-  };
+  blog.controller('PostsCtrl', [
+    '$scope', 'Post', function($scope, Post) {
+      $scope.posts = Post.query();
+      return $scope.create = function() {
+        return Post.save($scope.newPost, (function(resource) {
+          $scope.posts.push(resource);
+          return $scope.newPost = {};
+        }), function(response) {
+          return console.log("Error: " + response.status);
+        });
+      };
+    }
+  ]);
 
-  angular.bootstrap(document, ['angularExample']);
+  blog.factory('Post', [
+    '$resource', function($resource) {
+      var url;
+      url = "http://blog-test-angularjs.herokuapp.com/posts.json";
+      return $resource(url);
+    }
+  ]);
 
 }).call(this);
